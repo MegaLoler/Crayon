73b22cbb608aa357d95159b6d89fe3eda1af74d3
Aardbei <megaloler9000@gmail.com>
2019-08-04 09:10:17 -0500

    tryin a worker nvm

--- src/qt.cpp
+++ src/qt.cpp
@@ -6,7 +6,10 @@
 
 #include <QApplication>
 #include <QWidget>
+#include <QMouseEvent>
 #include <QPainter>
+#include <QThread>
+#include <QObject>
 
 using namespace std;
 
@@ -33,16 +36,129 @@ class QImage_Canvas : public Canvas {
         }
 };
 
+class Worker : public QObject {
+    Q_OBJECT
+
+    private:
+        QT_Canvas *window;
+
+    public:
+        Worker (QT_Canvas *window) : window (window) {}
+
+    public slots:
+        void process() {
+            Vec p1 = Vec (window->px, window->py);
+            Vec p2 = Vec (window->x, window->y);
+            window->canvas->stroke (p1, p2, window->crayon, window->max_force, window->smear);
+        }
+
+    signals:
+        void finished () {
+            window->update ();
+        }
+
+        void error (QString err) {
+            // print it or somethin idk
+        }
+};
+
 class QT_Canvas : public QWidget {
+    private:
+        bool mouse_down = false;
+        double px = 0;
+        double py = 0;
+        double x = 0;
+        double y = 0;
+        bool smear = false;
+        double max_force = 100;
+
+        // the different colored waxes
+        Wax *wax_red;
+        Wax *wax_orange;
+        Wax *wax_yellow;
+        Wax *wax_green;
+        Wax *wax_blue;
+        Wax *wax_purple;
+        Wax *wax_brown;
+        Wax *wax_black;
+        Wax *wax_grey;
+        Wax *wax_white;
+
+        // the crayons with those waxes
+        Crayon *crayon_red;
+        Crayon *crayon_orange;
+        Crayon *crayon_yellow;
+        Crayon *crayon_green;
+        Crayon *crayon_blue;
+        Crayon *crayon_purple;
+        Crayon *crayon_brown;
+        Crayon *crayon_black;
+        Crayon *crayon_grey;
+        Crayon *crayon_white;
+
+        // the current crayon
+        Crayon *crayon;
+
     public:
         QImage_Canvas *canvas;
 
-        QT_Canvas (QWidget *parent = 0) : QWidget (parent) {
-            canvas = new QImage_Canvas (default_width, default_height);
+        QT_Canvas (int width, int height, QWidget *parent = 0) : QWidget (parent) {
+            canvas = new QImage_Canvas (width, height);
+            resize (width, height);
+            setWindowTitle ("Crayon");
+            setMouseTracking (true);
+
+            // create the different colored waxes
+            wax_red    = new Wax (0.95,  0.45, 0.45,  0.605,  0.051);
+            wax_orange = new Wax (0.999, 0.55, 0.2,   0.605,  0.042);
+            wax_yellow = new Wax (0.95,  0.9,  0.2,   0.715,  0.111);
+            wax_green  = new Wax (0.35,  0.8,  0.35,  0.385,  0.096);
+            wax_blue   = new Wax (0.3,   0.5,  0.9,   0.715,  0.054);
+            wax_purple = new Wax (0.65,  0.4,  0.9,   0.385,  0.06);
+            wax_brown  = new Wax (0.8,   0.55, 0.5,   0.3025, 0.15);
+            wax_black  = new Wax (0.26,  0.25, 0.245, 0.11,   0.24);
+            wax_grey   = new Wax (0.42,  0.4,  0.39,  0.44,   0.51);
+            wax_white  = new Wax (0.8,   0.8,  0.79,  0.55,   0.33);
+
+            // create the crayons with those waxes
+            double diameter = 10;
+            crayon_red    = new Crayon (diameter, diameter, wax_red);
+            crayon_orange = new Crayon (diameter, diameter, wax_orange);
+            crayon_yellow = new Crayon (diameter, diameter, wax_yellow);
+            crayon_green  = new Crayon (diameter, diameter, wax_green);
+            crayon_blue   = new Crayon (diameter, diameter, wax_blue);
+            crayon_purple = new Crayon (diameter, diameter, wax_purple);
+            crayon_brown  = new Crayon (diameter, diameter, wax_brown);
+            crayon_black  = new Crayon (diameter, diameter, wax_black);
+            crayon_grey   = new Crayon (diameter, diameter, wax_grey);
+            crayon_white  = new Crayon (diameter, diameter, wax_white);
+
+            // set the current crayon
+            crayon = crayon_red;
         }
 
         ~QT_Canvas () {
-            //delete canvas;
+            delete canvas;
+            delete wax_red;
+            delete wax_orange;
+            delete wax_yellow;
+            delete wax_green;
+            delete wax_blue;
+            delete wax_purple;
+            delete wax_brown;
+            delete wax_black;
+            delete wax_grey;
+            delete wax_white;
+            delete crayon_red;
+            delete crayon_orange;
+            delete crayon_yellow;
+            delete crayon_green;
+            delete crayon_blue;
+            delete crayon_purple;
+            delete crayon_brown;
+            delete crayon_black;
+            delete crayon_grey;
+            delete crayon_white;
         }
 
     protected:
@@ -52,53 +168,40 @@ class QT_Canvas : public QWidget {
             QPainter painter (this);
             painter.drawImage (0, 0, canvas->get_image ());
         }
+
+        void mouseMoveEvent (QMouseEvent *event) {
+            x = event->x ();
+            y = event->y ();
+            if (mouse_down) {
+                QThread* thread = new QThread;
+                Worker* worker = new Worker (this);
+                worker->moveToThread (thread);
+                connect(worker, SIGNAL (error(QString)), this, SLOT (errorString(QString)));
+                connect(thread, SIGNAL (started()), worker, SLOT (process()));
+                connect(worker, SIGNAL (finished()), thread, SLOT (quit()));
+                connect(worker, SIGNAL (finished()), worker, SLOT (deleteLater()));
+                connect(thread, SIGNAL (finished()), thread, SLOT (deleteLater()));
+                thread->start();
+            }
+            px = x;
+            py = y;
+        }
+
+        void mousePressEvent (QMouseEvent *event) {
+            mouse_down = true;
+        }
+
+        void mouseReleaseEvent (QMouseEvent *event) {
+            mouse_down = false;
+        }
+
+    friend class Worker;
 };
 
 int main (int argc, char **argv) {
 
-    // create the different colored waxes
-    Wax *wax_red    = new Wax (0.95,  0.45, 0.45,  0.605,  0.051);
-    Wax *wax_orange = new Wax (0.999, 0.55, 0.2,   0.605,  0.042);
-    Wax *wax_yellow = new Wax (0.95,  0.9,  0.2,   0.715,  0.111);
-    Wax *wax_green  = new Wax (0.35,  0.8,  0.35,  0.385,  0.096);
-    Wax *wax_blue   = new Wax (0.3,   0.5,  0.9,   0.715,  0.054);
-    Wax *wax_purple = new Wax (0.65,  0.4,  0.9,   0.385,  0.06);
-    Wax *wax_brown  = new Wax (0.8,   0.55, 0.5,   0.3025, 0.15);
-    Wax *wax_black  = new Wax (0.26,  0.25, 0.245, 0.11,   0.24);
-    Wax *wax_grey   = new Wax (0.42,  0.4,  0.39,  0.44,   0.51);
-    Wax *wax_white  = new Wax (0.8,   0.8,  0.79,  0.55,   0.33);
-
-    // create the crayons with those waxes
-    double diameter = 10;
-    Crayon *crayon_red    = new Crayon (diameter, diameter, wax_red);
-    Crayon *crayon_orange = new Crayon (diameter, diameter, wax_orange);
-    Crayon *crayon_yellow = new Crayon (diameter, diameter, wax_yellow);
-    Crayon *crayon_green  = new Crayon (diameter, diameter, wax_green);
-    Crayon *crayon_blue   = new Crayon (diameter, diameter, wax_blue);
-    Crayon *crayon_purple = new Crayon (diameter, diameter, wax_purple);
-    Crayon *crayon_brown  = new Crayon (diameter, diameter, wax_brown);
-    Crayon *crayon_black  = new Crayon (diameter, diameter, wax_black);
-    Crayon *crayon_grey   = new Crayon (diameter, diameter, wax_grey);
-    Crayon *crayon_white  = new Crayon (diameter, diameter, wax_white);
-
-    // set the current crayon
-    Crayon *crayon = crayon_red;
-
-    //canvas.stroke (Vec (px, py), Vec (x, y), crayon, max_force * event.tfinger.pressure, smear);
-    double px = 0;
-    double py = 0;
-    double x = 0;
-    double y = 0;
-    bool quit = false;
-    bool smear = false;
-    double max_force = 100;
-
-    // the qt gui
     QApplication app (argc, argv);
-    QT_Canvas window;
-
-    window.resize (default_width, default_height);
-    window.setWindowTitle ("Crayon");
+    QT_Canvas window (default_width, default_height);
     window.show ();
 
     return app.exec ();
